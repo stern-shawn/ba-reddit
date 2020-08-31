08d3c508100d67556f1be3b555b88851f3d9fcf1
Shawn Stern <stern.shawn@gmail.com>
2020-08-30 19:36:31 -0700

    feat(orm): add migration + creation and login resolvers for User type

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import path from 'path';
 import { MikroORM } from '@mikro-orm/core';
 import { Post } from './entities/Post';
 import { isProd } from './constants';
+import { User } from './entities/User';
 
 export default {
   migrations: {
     path: path.join(__dirname, './migrations'), // path to the folder with migrations
     pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files (ts or js)
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: 'bareddit',
   type: 'postgresql',
   debug: !isProd,
---
+++ src/resolvers/user.ts
@@ -0,0 +1,103 @@
+import argon2 from 'argon2';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  InputType,
+  Field,
+  Ctx,
+  ObjectType,
+} from 'type-graphql';
+import { MyContext } from '../types';
+import { User } from '../entities/User';
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          { field: 'username', message: 'Length must be greater than 2' },
+        ],
+      };
+    }
+
+    if (options.password.length <= 3) {
+      return {
+        errors: [
+          { field: 'password', message: 'Length must be greater than 3' },
+        ],
+      };
+    }
+
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+
+    try {
+      await em.persistAndFlush(user);
+    } catch (e) {
+      console.log(e);
+      if (e.code === '23505' || e.detail.includes('already exists')) {
+        // Duplicate username
+        return {
+          errors: [{ field: 'username', message: 'username already taken' }],
+        };
+      }
+    }
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [{ field: 'username', message: "That username doesn't exist" }],
+      };
+    }
+
+    const isValid = await argon2.verify(user.password, options.password);
+    if (!isValid)
+      return {
+        errors: [{ field: 'password', message: 'Incorrect password' }],
+      };
+
+    return { user };
+  }
+}
