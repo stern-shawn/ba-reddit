dcc744f7b499e296d8228d04bf9f3c1955b3829d
Shawn Stern <stern.shawn@gmail.com>
2020-09-01 22:24:14 -0700

    feat(ui): reorg files and add basic nextjs app w chakra-ui

---
+++ web/src/pages/register.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import { Box, Button } from '@chakra-ui/core';
+import Wrapper from '../components/Wrapper';
+import InputField from '../components/InputField';
+
+interface RegisterProps {}
+
+const Register: React.FC<RegisterProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={(values) => {
+          console.log('values: ', values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt="4">
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              type="submit"
+              variantColor="teal"
+              mt={4}
+              isLoading={isSubmitting}
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
