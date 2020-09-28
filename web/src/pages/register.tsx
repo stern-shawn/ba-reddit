d42de56a3311e7173a2e97389925107c5485ec7e
Shawn Stern <stern.shawn@gmail.com>
2020-09-27 22:42:54 -0700

    feat(user): add required email field and ability to login with username or email, refactor validation and ui forms

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -18,9 +18,9 @@ const Register: React.FC<RegisterProps> = ({}) => {
   return (
     <Wrapper variant="small">
       <Formik
-        initialValues={{ username: '', password: '' }}
+        initialValues={{ email: '', username: '', password: '' }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await register(values);
+          const response = await register({ options: values });
 
           if (response.data?.register.errors) {
             setErrors(toErrorMap(response.data.register.errors));
@@ -36,6 +36,9 @@ const Register: React.FC<RegisterProps> = ({}) => {
               placeholder="username"
               label="Username"
             />
+            <Box mt="4">
+              <InputField name="email" placeholder="email" label="Email" />
+            </Box>
             <Box mt="4">
               <InputField
                 name="password"
