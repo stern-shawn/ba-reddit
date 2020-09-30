3661dfda2c08b57c21d8948b268d458d724d502b
Shawn Stern <stern.shawn@gmail.com>
2020-09-29 21:12:35 -0700

    feat: scaffold the change password form

--- web/src/pages/change-password/[token].tsx
+++ web/src/pages/change-password/[token].tsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import { Button } from '@chakra-ui/core';
+import { Form, Formik } from 'formik';
 import { NextPage } from 'next';
+import React from 'react';
+
+import InputField from '../../components/InputField';
+import Wrapper from '../../components/Wrapper';
+import { toErrorMap } from '../../utils/toErrorMap';
 
 interface ChangePasswordProps {
   token: string;
 }
 
 const ChangePassword: NextPage<ChangePasswordProps> = ({ token }) => {
-  return <div>token is {token}</div>;
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ newPassword: '' }}
+        onSubmit={async (values, { setErrors }) => {
+          // const response = await changePassword(token, values.newPassword);
+          // if (response.data?....) {
+          //   setErrors(toErrorMap(response.data....));
+          // } else if (response.data?....) {
+          //   router.push('/');
+          // }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="newPassword"
+              placeholder="New Password"
+              label="New Password"
+              type="password"
+            />
+            <Button
+              type="submit"
+              variantColor="teal"
+              mt={4}
+              isLoading={isSubmitting}
+            >
+              Change Password
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 };
 
 ChangePassword.getInitialProps = ({ query }) => {
